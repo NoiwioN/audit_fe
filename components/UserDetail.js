bb15418543df68961de1e35ae8da9a9eb569e15d
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-03-21 11:50:18 +0100

    Added Authentication

---
+++ components/UserDetail.js
@@ -0,0 +1,12 @@
+export default function UserDetail({user}) {
+    return !user ? <p>User...</p> : (
+        <div>
+            <div>
+                <h1>Benutzername: {user.benutzername}</h1>
+                <p>Vorname: {user.vorname}</p>
+                <p>Nachname: {user.nachname}</p>
+                <p>Email: {user.email}</p>
+            </div>
+        </div>)
+}
+
