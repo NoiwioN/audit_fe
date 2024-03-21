9f15b9e5a68455454e246da8f94877fd2fd6dc2b
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-03-21 13:23:35 +0100

    added Users-Page

--- components/Users.js
+++ components/Users.js
@@ -1,18 +1,22 @@
 import {useGlobalContext} from "@/store";
 import Link from "next/link";
+import UsersAPI from "@/lib/api/Users";
 
 export default function Users ({nachname, name, email, benutzername, id}) {
 
     const {session} = useGlobalContext();
+    const handleDelete = async ()=>{
+        await UsersAPI.delete(id,session.accessToken);
+    }
 
     return (
         <div>
-            <h1>{benutzername}</h1>
+            <h2>{benutzername}</h2>
             <p>{email}</p>
             <p>{nachname} {nachname}</p>
             <Link href={`users/${id}`}>Mehr Details</Link>
             {session&&(<Link href={`users/edit/${id}`}>Bearbeiten</Link>)}
-            {session&&(<Link href={`users/edit/${id}`}>Bearbeiten</Link>)}
+            {session&&(<button onClick={()=>{handleDelete().then(window.location.reload)}}>Bearbeiten</button>)}
         </div>
     )
 }
