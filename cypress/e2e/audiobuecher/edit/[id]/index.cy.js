7c573b1cc2a042ca7eb7691876d517e16ef5e1fa
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-03-28 10:23:19 +0100

    Alle Test ausgeführt

--- cypress/e2e/audiobuecher/edit/[id]/index.cy.js
+++ cypress/e2e/audiobuecher/edit/[id]/index.cy.js
@@ -2,7 +2,7 @@
 describe('Testfall 3', () => {
     it('Der Autor des Buchs wurde verändert. ', () => {
         const title= "Mastering the Art of French Cooking"
-        const newAuthor = Math.random().toString()
+        const newAuthor = Math.random().toString().slice(2,8);
         cy.visit('http://localhost:3000/')
         cy.contains("Login").click()
         cy.url().should('include','/login')
@@ -13,6 +13,7 @@ describe('Testfall 3', () => {
         cy.get("h1").contains(title)
             .parent().contains("Mehr Details").click()
         cy.contains("Bearbeiten").click();
+        cy.get("[name='autor']").clear()
         cy.get("[name='autor']").type(newAuthor)
         cy.contains("Erstellen").click();
         cy.get("h1").contains(title).parent()
@@ -21,6 +22,7 @@ describe('Testfall 3', () => {
         cy.get("h1").contains(title)
             .parent().contains("Mehr Details").click()
         cy.contains("Bearbeiten").click();
+        cy.get("[name='autor']").clear()
         cy.get("[name='autor']").type("Julia Child")
         cy.contains("Erstellen").click();
     })
